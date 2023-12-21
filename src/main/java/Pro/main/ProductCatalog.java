d44e3c9a604ff7a28fcb67380b29b7d1104baf63
Walaessam27 <145562141+Walaessam27@users.noreply.github.com>
2023-12-21 22:12:32 +0200

    commit15

--- src/main/java/Pro/main/ProductCatalog.java
+++ src/main/java/Pro/main/ProductCatalog.java
@@ -4,64 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProductCatalog {
-    public String[][] interiorProducts;
-    public String[][] exteriorProducts;
-    public String[][] electronicsProducts;
+    private List<String[][]> productCategories;
 
-    public ProductCatalog(int maxInterior, int maxExterior, int maxElectronics) {
-        this.interiorProducts = new String[maxInterior][4]; // Assuming 4 columns for product listing: name, description, price, availability
-        this.exteriorProducts = new String[maxExterior][4];
-        this.electronicsProducts = new String[maxElectronics][4];
+    public ProductCatalog() {
+        this.productCategories = new ArrayList<>();
     }
 
-    public void addInteriorProduct(String name, String description, String price, String availability) {
-        for (int i = 0; i < interiorProducts.length; i++) {
-            if (interiorProducts[i][0] == null) {
-                interiorProducts[i] = new String[]{name, description, price, availability};
+    public void addProductCategory(int maxProducts, String categoryName) {
+        String[][] newCategory = new String[maxProducts][4];
+        productCategories.add(newCategory);
+        System.out.println("New product category '" + categoryName + "' added.");
+    }
+
+    public void addProduct(String categoryName, String name, String description, String price, String availability) {
+        for (String[][] category : productCategories) {
+            if (category[0][0] != null && category[0][0].equalsIgnoreCase(categoryName)) {
+                for (int i = 0; i < category.length; i++) {
+                    if (category[i][0] == null) {
+                        category[i] = new String[]{name, description, price, availability};
+                        System.out.println("Product added to category '" + categoryName + "'.");
+                        return;
+                    }
+                }
+                System.out.println("Category '" + categoryName + "' is full. Cannot add more products.");
                 return;
             }
         }
-        System.out.println("Interior product category is full. Cannot add more products.");
-    }
-
-    public void addExteriorProduct(String name, String description, String price, String availability) {
-        for (int i = 0; i < exteriorProducts.length; i++) {
-            if (exteriorProducts[i][0] == null) {
-                exteriorProducts[i] = new String[]{name, description, price, availability};
-                return;
-            }
-        }
-        System.out.println("Exterior product category is full. Cannot add more products.");
-    }
-
-    public void addElectronicsProduct(String name, String description, String price, String availability) {
-        for (int i = 0; i < electronicsProducts.length; i++) {
-            if (electronicsProducts[i][0] == null) {
-                electronicsProducts[i] = new String[]{name, description, price, availability};
-                return;
-            }
-        }
-        System.out.println("Electronics product category is full. Cannot add more products.");
+        System.out.println("Category '" + categoryName + "' not found.");
     }
 
     public String[][] searchProducts(String keyword) {
-        List<String[]> foundProducts = new ArrayList<String[]>();
-        for (String[] product : interiorProducts) {
-            if (product[0] != null && (product[0].toLowerCase().contains(keyword.toLowerCase()) ||
-                    product[1].toLowerCase().contains(keyword.toLowerCase()))) {
-                foundProducts.add(product);
-            }
-        }
-        for (String[] product : exteriorProducts) {
-            if (product[0] != null && (product[0].toLowerCase().contains(keyword.toLowerCase()) ||
-                    product[1].toLowerCase().contains(keyword.toLowerCase()))) {
-                foundProducts.add(product);
-            }
-        }
-        for (String[] product : electronicsProducts) {
-            if (product[0] != null && (product[0].toLowerCase().contains(keyword.toLowerCase()) ||
-                    product[1].toLowerCase().contains(keyword.toLowerCase()))) {
-                foundProducts.add(product);
+        List<String[]> foundProducts = new ArrayList<>();
+        for (String[][] category : productCategories) {
+            for (String[] product : category) {
+                if (product[0] != null && (product[0].toLowerCase().contains(keyword.toLowerCase()) ||
+                        product[1].toLowerCase().contains(keyword.toLowerCase()))) {
+                    foundProducts.add(product);
+                }
             }
         }
         return foundProducts.toArray(new String[0][]);
