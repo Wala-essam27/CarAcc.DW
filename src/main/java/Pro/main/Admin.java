01a8be8aebc812b2ab7cdd84ca80b1004c25c038
Walaessam27 <145562141+Walaessam27@users.noreply.github.com>
2023-12-22 15:32:00 +0200

    commit16

--- src/main/java/Pro/main/Admin.java
+++ src/main/java/Pro/main/Admin.java
@@ -3,30 +3,56 @@ package Pro.main;
 import java.util.Scanner;
 
 public class Admin {
+	
+	
 	public static void list()
 { 
-	
+    ProductCatalog catalog = new ProductCatalog();
 	Scanner scanner = new Scanner(System.in);
 while (true) {
-    System.out.println("Admin Menu:");
-    System.out.println("1. Manage product categories");
+	
+    System.out.println("\n Admin Menu:");
+    System.out.println("1. Show product categories");
     System.out.println("2. Add and update product listings");
-    System.out.println("3. View and manage customer accounts");
-    System.out.println("4. Schedule and manage installation appointments");
+    System.out.println("3. Add a new catagory.");
+    System.out.println("4. View and manage customer accounts");
+    System.out.println("5. Schedule and manage installation appointments");
     System.out.println("0. Exit");
 
-    System.out.print("Enter your choice: ");
+    System.out.print("\n Enter your choice: ");
     int choice = scanner.nextInt();
 
     switch (choice) {
+    
         case 1:
+        	
+             catalog.viewAllProducts();
            
             break;
         case 2:
-          
-            break;
+        	  System.out.println("Enter product details:");
+              scanner.nextLine(); // Consume the newline character
+              System.out.print("Category Name: ");
+              String categoryName = scanner.nextLine();
+              System.out.print("Product Name: ");
+              String name = scanner.nextLine();
+              System.out.print("Description: ");
+              String description = scanner.nextLine();
+              System.out.print("Price: ");
+              String price = scanner.nextLine();
+              System.out.print("Availability: ");
+              String availability = scanner.nextLine();
+
+              catalog.addProduct(categoryName, name, description, price, availability);
+              break;
         case 3:
-           
+        	System.out.println("Enter Category details:");
+            scanner.nextLine(); // Consume the newline character
+            System.out.print("Category Name: ");
+            String categoryNam = scanner.nextLine();
+            System.out.print("max Size ");
+            int size = Integer. parseInt(scanner.nextLine());
+        	catalog.addProductCategory(size, categoryNam);
             break;
         case 4:
            
--- src/main/java/Pro/main/ProductCatalog.java
+++ src/main/java/Pro/main/ProductCatalog.java
@@ -2,19 +2,61 @@ package Pro.main;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ProductCatalog {
     private List<String[][]> productCategories;
 
     public ProductCatalog() {
-        this.productCategories = new ArrayList<>();
+    	 this.productCategories = new ArrayList<>();
+
+        // Initialize with some initial values (example)
+        String[][] InteriorCategory = new String[][] {
+        	 {"Interior", null, null, null},
+             {"Seat Covers", "Custom-fit seat covers for comfort", "$29.99", "In Stock"},
+             {"Floor Mats", "Durable and easy to clean floor mats", "$19.99", "In Stock"},
+             {"Steering Wheel Cover", "Stylish cover for a comfortable grip", "$14.99", "In Stock"},
+             {null, null, null, null}
+        };
+
+        String[][] ExteriorCategory = new String[][] {
+        	{"Exterior", null, null, null},
+            {"Car Wax", "Protects and enhances the car's exterior shine", "$9.99", "In Stock"},
+            {"Windshield Wipers", "High-quality wipers for clear visibility", "$7.99", "In Stock"},
+            {"License Plate Frame", "Customizable frame for a personal touch", "$5.99", "In Stock"},
+            {null, null, null, null}
+        };
+        
+        String[][] ElectronicsCategory = new String[][] {
+        	 {"Electronics", null, null, null},
+             {"Car Charger", "Dual USB port charger for devices", "$12.99", "In Stock"},
+             {"Bluetooth Car Kit", "Hands-free calling and music streaming", "$24.99", "In Stock"},
+             {"GPS Navigation System", "Turn-by-turn directions for easy navigation", "$79.99", "In Stock"},
+             {null, null, null, null}
+        };
+
+        // Add initialized categories to the list
+        productCategories.add(ElectronicsCategory);
+        productCategories.add(ExteriorCategory);
+        productCategories.add(InteriorCategory);
     }
+
 
     public void addProductCategory(int maxProducts, String categoryName) {
         String[][] newCategory = new String[maxProducts][4];
         productCategories.add(newCategory);
-        System.out.println("New product category '" + categoryName + "' added.");
-    }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter product details:");
+        System.out.print("Product Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Description: ");
+        String description = scanner.nextLine();
+        System.out.print("Price: ");
+        String price = scanner.nextLine();
+        System.out.print("Availability: ");
+        String availability = scanner.nextLine();
+        addProduct(categoryName, name, description, price, availability);
+        System.out.println("New product category '" + categoryName + "' added.");}
 
     public void addProduct(String categoryName, String name, String description, String price, String availability) {
         for (String[][] category : productCategories) {
@@ -32,7 +74,29 @@ public class ProductCatalog {
         }
         System.out.println("Category '" + categoryName + "' not found.");
     }
+    public void viewAllProducts() {
+        if (productCategories.isEmpty()) {
+            System.out.println("No products in the catalog.");
+            return;
+        }
 
+        System.out.println("All Products in the Catalog:");
+
+        for (String[][] category : productCategories) {
+            for (String[] product : category) {
+            	
+                if (product[0] != null && product[1] != null&& product[2] != null&& product[3] != null) {
+                	System.out.println("Category: " + category[0][0]);
+                    System.out.println("Product: " + product[0]);
+                    System.out.println("Description: " + product[1]);
+                    System.out.println("Price: " + product[2]);
+                    System.out.println("Availability: " + product[3]);
+                    System.out.println("------------------------");
+                }
+            }
+        }
+    }
+    
     public String[][] searchProducts(String keyword) {
         List<String[]> foundProducts = new ArrayList<>();
         for (String[][] category : productCategories) {
